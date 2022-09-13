ba5bb9fcf55edef4401e2f34868661421f6461f0
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-09-13 13:53:03 -0500

    Update getAllRecipes.js

--- routes/getAllRecipes.js
+++ routes/getAllRecipes.js
@@ -2,15 +2,18 @@ const { Router, query } = require('express');
 // const { data } = require('../utils/utils')
 const router = Router();
 
-const rec = {
-    id:1,
-    name: "Cafe",
-    diet:['vegetarian'],
-    score:5,
-    image:'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
-    summary: "bebida saludable y energetica facil de preparar",
-    steps:"comprar el cafe y el azucar, hervir el agua y mezclar todos los ingredientes en proporcion 1a x 4c "
-}
+const rec = [
+    {
+        id:1,
+        name: "Cafe",
+        diet:['vegetarian'],
+        score:5,
+        image:'https://upload.wikimedia.org/wikipedia/commons/4/45/A_small_cup_of_coffee.JPG',
+        summary: "bebida saludable y energetica facil de preparar",
+        steps:"comprar el cafe y el azucar, hervir el agua y mezclar todos los ingredientes en proporcion 1a x 4c "
+    }
+]
+
 
 
 router.get('/', (req, res) => {
