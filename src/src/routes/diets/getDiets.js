9e0efb4d8c422496e602e028137821d772edf8de
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-10-19 17:26:32 -0500

    Update getDiets.js

--- src/src/routes/diets/getDiets.js
+++ src/src/routes/diets/getDiets.js
@@ -3,16 +3,25 @@ const { types} = require ('../../utils/dietTypes.js')
 
 module.exports.getDietTypes = async (req,res) => {
 
-   
-    // console.log( types)
-    
+  try {
 
-      
-      
-  } catch (error) {
-    console.log(error);
-  }
-  
+    let dietsDB = await Diets.findAll()
+    if (!dietsDB.length) { console.log('no diets')}
+         
+    let diets= types.map(async (d) => {
+      await Diets.findOrCreate({
+        where: { name: d },
+      });
+    });
+    
+      await Promise.all(diets)
+    let response = await Diets.findAll()
+    // console.log(response.map(r=>r))
+    return res.json(response)
+    
+} catch (error) {
+  console.log(error);
+}
 
   
 };
