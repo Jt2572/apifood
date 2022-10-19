1db9384c63cacc08c4892551b650b96008fff21c
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2022-10-19 14:00:58 -0500

    Update getRecipes.js

--- src/routes/getRecipes/getRecipes.js
+++ src/routes/getRecipes/getRecipes.js
@@ -1,31 +1,18 @@
-require("dotenv").config();
-const { YOUR_API_KEY } = process.env;
-const axios = require("axios");
-const food = require('./food.json');
-  const { toId } = require('../../utils/toId.js')
-const { Recipe, Diets } = require("../../db");
-const { Op } = require("sequelize");
-const { types } = require('../../utils/dietTypes.js')
+
 
 module.exports.getRecipes = async (req, res) => {
   const { name } = req.query;  
-  var recsCreated = []
+
   var resp = []
   
   try {
-      resp = await Diets.findAll()
+      resp='getRecipes'
 
 
     } catch (error) {
       console.log(error.message)
     }  
 
-   
-
-    
-    
-    
- 
     
     
   return res.json(resp)
