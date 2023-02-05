0e112ce4961f65276d59e6615b9ffeaa451dabde
Jt2572 <95650277+Jt2572@users.noreply.github.com>
2023-02-05 12:04:45 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,13 +20,12 @@
 
 const server = require('./app.js');
 const { conn } = require('./db.js');
+const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') })
 
 const port = process.env.PORT || 3001;
-// Syncing all the models at once.
-
-conn.sync({ force:true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(port, () => {
-    console.log(`%s listening at ${port} `); // eslint-disable-line no-console
+    console.log(`Listening at ${port} ` ); 
   });
 });
